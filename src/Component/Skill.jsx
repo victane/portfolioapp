d519db1fee36ad9113af25421daa0e70234906fc
victane <sundayvictane@gmail.com>
2023-02-18 05:29:06 -0500

    two skillset added to the list of skills in Skills Component

--- src/Component/Skill.jsx
+++ src/Component/Skill.jsx
@@ -4,13 +4,15 @@ const Skill = () => {
     return (
         <div className='skill-span'>
 
-            <span > Skills</span>
+            <span style={{ fontSize: '1.2rem' }}> Skills</span>
             <ul>
                 <li>Javascript</li>
                 <li>React</li>
                 <li>Git</li>
                 <li>Figma</li>
                 <li>Html</li>
+                <li>css</li>
+                <li>NodeJs</li>
             </ul>
 
         </div>
